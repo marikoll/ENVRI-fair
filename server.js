896dec9b9b38ea58e39eb62862ab7636253461d7
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2021-10-07 09:18:44 +0200

    with test URLs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // Config
 const port = 3000;
 
-// 
+// Load installed modules (npm install). Q: difference between const and var?
 var fs     = require('fs');
 var xmlParser = require("xml2json");
 var express = require("express");
@@ -11,17 +11,21 @@ var express = require("express");
 // e.g. MongoDB that stores JSON
 // Here we just read an XML file and parse it to JSON.
 let filename = __dirname+'/eml_aegean_plychaetes.xml';
-console.log(filename);
+console.log("Parsing " + filename);
 const xmlData = fs.readFileSync(filename);
 // Parse to JSON object
 var jsonObj=JSON.parse(xmlParser.toJson(xmlData));
 
 
-// The web request handler
+// Set up a webapp. Q new needed or not?
 var app = new express();
 
-// 
+
+
+// Set up routes
+// author name
 app.get('/api/v1/:datasetId/author', function(req,res){
+    // TODO add search for datasetId  in req.params["datasetId"]
     res.format({
         text: () => {
             res.send(jsonObj["eml:eml"]["dataset"]["creator"]["individualName"]["givenName"] + " " +
@@ -33,7 +37,7 @@ app.get('/api/v1/:datasetId/author', function(req,res){
     });
 });
 
-// just an example
+// Time
 app.get('/api/v1/:datasetId/time', function(req,res){
     res.format({
         text: () => {
@@ -45,5 +49,17 @@ app.get('/api/v1/:datasetId/time', function(req,res){
     });
 });
 
+// Security test route
+app.get('/api/v1/:datasetId/testing', function(req,res){
+    res.format({
+        text: () => {
+            res.send("http://bad.guy.somewhere/format_client_disk.html");
+        },
+        html: () => {
+            res.send("<p>You asked for this</p><a href=\"http://do.bad.stuff/empty_my_account_script.html\">Good luck</a></p>");
+        }
+    });
+});
 
+// Start the app on port
 app.listen(port, () => console.log(`FAIR app listening on port ${port}!`));
